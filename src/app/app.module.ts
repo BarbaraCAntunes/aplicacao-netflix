5d8425464f96235590457e6d960080ca86e43162
Barbara <barbaracantunes@gmail.com>
2023-03-12 14:37:54 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardSerieComponent } from './components/card-serie/card-serie.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardSerieComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card-serie/card-serie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-serie',
+  templateUrl: './card-serie.component.html',
+  styleUrls: ['./card-serie.component.scss']
+})
+export class CardSerieComponent {
+
+}
