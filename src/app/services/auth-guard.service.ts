65d9c5729757231a6661ac90acc938469b0ed6d0
Barbara <barbaracantunes@gmail.com>
2023-03-12 17:02:13 -0300

    finaliza componente login e proteção de rotas

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authService.isLoggedIn()) {
-      return true;
+      return this.router.parseUrl('/');
     } else {
       this.router.navigate(['/login']);
       return false;
